23bdbade5ace460db32d52fa9b357767088ffb50
College-admission <dacgadmission@gmail.com>
2026-03-10 07:42:13 +0530

    Completed Task 3 and Task 4

--- src/main/java/com/jpmc/midascore/component/KafkaConsumer.java
+++ src/main/java/com/jpmc/midascore/component/KafkaConsumer.java
@@ -3,19 +3,19 @@ package com.jpmc.midascore.component;
 import com.jpmc.midascore.foundation.Transaction;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Component
 public class KafkaConsumer {
-    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
 
-    // Yahan humne topic ka naam hardcode kar diya hai fix karne ke liye
+    private final DatabaseConduit databaseConduit;
+
+    public KafkaConsumer(DatabaseConduit databaseConduit) {
+        this.databaseConduit = databaseConduit;
+    }
+
     @KafkaListener(topics = "trader-updates", groupId = "midas-group")
     public void listen(Transaction transaction) {
-        
-        System.out.println("Received Transaction Amount: " + transaction.getAmount());
-        
-        logger.info("Received Transaction: {}", transaction);
+        // Ab hum print karne ke bajaye database function ko bhej rahe hain
+        databaseConduit.processTransaction(transaction);
     }
 }
