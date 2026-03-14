4cbe57d446ece0f911e62b6229a63862546be5ad
College-admission <dacgadmission@gmail.com>
2026-03-14 15:52:02 +0530

    Completed Task 4: Implemented Balance API

--- src/main/java/com/jpmc/midascore/controller/BalanceController.java
+++ src/main/java/com/jpmc/midascore/controller/BalanceController.java
@@ -2,8 +2,12 @@ package com.jpmc.midascore.controller;
 
 import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Balance;
+import com.jpmc.midascore.foundation.Incentive;
+import com.jpmc.midascore.foundation.Transaction;
 import com.jpmc.midascore.repository.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +31,9 @@ public Balance getBalance(@RequestParam(required = false) Long userId) {
     }
     return new Balance(0f);
 }
+
+    @PostMapping("/incentive")
+    public Incentive getIncentive(@RequestBody Transaction transaction) {
+        return new Incentive();
+    }
 }
